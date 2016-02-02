5290c4e35b1927b8b88ea223660f1f09515c17bb
Andrey Lisin <andrey.lisin@gmail.com>
2016-02-02 17:17:59 +0600

    Initial commit

---
+++ 01/main.hs
@@ -0,0 +1,26 @@
+charToString :: Char -> String
+charToString ch = [ch]
+
+readDigit :: Char -> Integer
+readDigit x = read $ charToString x
+
+toDigits :: Integer -> [Integer]
+toDigits x
+  | x <= 0 = []
+  | otherwise = map readDigit x'
+    where x' = show x
+
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev = reverse . toDigits
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther (x:y:zs) =
+  [x, y+y] ++ doubleEveryOther zs
+doubleEveryOther [x] = [x]
+doubleEveryOther _ = []
+
+sumDigits :: [Integer] -> Integer
+sumDigits xs = sum . concat $ map toDigits xs
+
+validate :: Integer -> Bool
+validate x = (sumDigits . doubleEveryOther . toDigitsRev $ x) `rem` 10 == 0
