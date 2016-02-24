3c51e938c5d9aa7aa30f774c069674c8e3aeb510
Andrey Lisin <andrey.lisin@gmail.com>
2016-02-24 20:09:10 +0600

    Add solution for hanoi puzzle

--- 01/hanoi.hs
+++ 01/hanoi.hs
@@ -6,6 +6,7 @@ type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi numberOfDisks pegA pegB pegC
-  | numberOfDisks == 1 = [(pegA, pegB)]
-  | numberOfDisks == 2 = [(pegA, pegC), (pegA,pegB), (pegC,pegB)]
+hanoi numberOfDisks fromPeg toPeg tempPeg
+  | numberOfDisks == 1 = [(fromPeg, toPeg)]
+  | numberOfDisks == 2 = [(fromPeg, tempPeg), (fromPeg, toPeg), (tempPeg, toPeg)]
+  | otherwise = hanoi (numberOfDisks - 1) fromPeg tempPeg toPeg ++ [(fromPeg, toPeg)] ++ hanoi (numberOfDisks - 1) tempPeg toPeg fromPeg
