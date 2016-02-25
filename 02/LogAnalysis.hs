e2a6f3f35f8d8b99a7fc84f8b308a02355ad05d5
Andrey Lisin <andrey.lisin@gmail.com>
2016-02-25 17:02:50 +0600

    Add parseMessage function

--- 02/LogAnalysis.hs
+++ 02/LogAnalysis.hs
@@ -3,3 +3,12 @@
 module LogAnalysis where
 
 import Log
+
+parseMessage :: String -> LogMessage
+parseMessage message =
+  let messageChunks = words message in
+    case messageChunks of
+      (["E", priority, timestamp, description]) -> LogMessage (Error (read priority)) (read timestamp) description
+      (["W", timestamp, description]) -> LogMessage Warning (read timestamp) description
+      (["I", timestamp, description]) -> LogMessage Info (read timestamp) description
+      _ -> Unknown message
