e2c978e950acbbb248efe151ce699738bf28443c
Andrey Lisin <andrey.lisin@gmail.com>
2016-02-26 16:53:48 +0600

    Able to parse log messages

--- 02/LogAnalysis.hs
+++ 02/LogAnalysis.hs
@@ -6,9 +6,11 @@ import Log
 
 parseMessage :: String -> LogMessage
 parseMessage message =
-  let messageChunks = words message in
+  let
+    messageChunks = words message
+    restoreString = foldl1 (\acc word -> acc ++ " " ++ word) in
     case messageChunks of
-      (["E", priority, timestamp, description]) -> LogMessage (Error (read priority)) (read timestamp) description
-      (["W", timestamp, description]) -> LogMessage Warning (read timestamp) description
-      (["I", timestamp, description]) -> LogMessage Info (read timestamp) description
+      ("E":priority:timestamp:description) -> LogMessage (Error (read priority)) (read timestamp) (restoreString description)
+      ("W":timestamp:description) -> LogMessage Warning (read timestamp) (restoreString description)
+      ("I":timestamp:description) -> LogMessage Info (read timestamp) (restoreString description)
       _ -> Unknown message
